34c27e7df4caac305df3aa9ea04936c1e848b05d
Germano Nascimento <nascimento.germano@gmail.com>
2025-03-11 22:14:44 -0300

    Aprimorando o trabalho com pandas

--- carros.py
+++ carros.py
@@ -10,3 +10,13 @@ print(df_filtrado_marca)
 df_filtrado_valor = df[df['valor'] > 98000]
 print()
 print(df_filtrado_valor)
+
+df_1 = df[['modelo', 'ano_fabricacao', 'valor']]
+ano_maximo = df_1['ano_fabricacao'].max()
+df_1_mais_novo = df_1[df_1['ano_fabricacao'] == ano_maximo]
+print()
+print(df_1_mais_novo)
+
+valor_maximo = df_1_mais_novo['valor'].max()
+df_1_maior_valor = df_1_mais_novo[df_1_mais_novo['valor'] == valor_maximo]
+print(df_1_maior_valor)
